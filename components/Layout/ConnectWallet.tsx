d16d6c21891464fc5203680fae84b59981d86e7d
starwalker00 <aaronwalkerup@gmail.com>
2022-04-08 16:25:37 +0200

    chore: restyle connect wallet

--- components/Layout/ConnectWallet.tsx
+++ components/Layout/ConnectWallet.tsx
@@ -1,10 +1,40 @@
 import { useStarknet, InjectedConnector } from '@starknet-react/core'
+import {
+    Button,
+    Spacer,
+    Stack,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem
+} from '@chakra-ui/react';
+import { ChevronDownIcon, SmallCloseIcon } from '@chakra-ui/icons';
+import { truncateEthAddress } from 'lib/helper';
 
 export function ConnectWallet() {
-    const { account, connect } = useStarknet()
+    const { account, connect, } = useStarknet()
 
     if (account) {
-        return <p>Account: {account}</p>
+        // return <p>Account: {account}</p>
+        return (
+            <Menu>
+                <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                    {truncateEthAddress(account)}
+                </MenuButton>
+                <MenuList>
+                    <MenuItem>
+                        <Stack width={'full'}
+                            direction='row'
+                            alignItems='baseline'
+                            justifyContent='space-between'
+                        >
+                            <span>Logout</span>
+                            <SmallCloseIcon />
+                        </Stack>
+                    </MenuItem>
+                </MenuList>
+            </Menu >
+        )
     }
 
     return <button onClick={() => connect(new InjectedConnector())}>Connect</button>
