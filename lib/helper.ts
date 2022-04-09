564241fb77b5c5b67c6112ae86360a467e84cec8
starwalker00 <aaronwalkerup@gmail.com>
2022-04-09 16:53:35 +0200

    chore: add namedconsolelog

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,3 +1,8 @@
+export const namedConsoleLog = (variableName: string, variableValue: any) => {
+    console.log(`${variableName}:::::`);
+    console.log(variableValue);
+};
+
 export const truncateEthAddress = (address: string) => {
     const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
     const match = address.match(truncateRegex);
