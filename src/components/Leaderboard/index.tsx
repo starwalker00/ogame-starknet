472cd89090675623f9097ba4777729b0468a9c24
starwalker00 <aaronwalkerup@gmail.com>
2022-04-10 17:17:06 +0200

    chore: remove unused code

--- src/components/Leaderboard/index.tsx
+++ src/components/Leaderboard/index.tsx
@@ -1,26 +1,4 @@
-import { InferGetStaticPropsType } from 'next'
 
-type Post = {
-    author: string
-    content: string
+export default function Leaderboard() {
+    return null
 }
-
-export const getStaticProps = async () => {
-    console.log("getStaticProps")
-
-    const res = await fetch('https://.../posts')
-    const posts: Post[] = await res.json()
-
-    return {
-        props: {
-            posts,
-        },
-    }
-}
-
-function Leaderboard({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
-    // will resolve posts to type Post[]
-    return (<h1>dfgf</h1>)
-}
-
-export default Leaderboard
