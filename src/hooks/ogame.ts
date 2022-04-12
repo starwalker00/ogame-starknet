2857064ae3ada07c6f8a3e718e66acb30ca8ec41
starwalker00 <aaronwalkerup@gmail.com>
2022-04-12 20:42:50 +0200

    chore: put contract addresses to env var

--- src/hooks/ogame.ts
+++ src/hooks/ogame.ts
@@ -6,6 +6,6 @@ import OgameAbi from 'src/abi/ogame.json'
 export function useOgameContract() {
     return useContract({
         abi: OgameAbi as Abi,
-        address: '0x03763a8330144f3552ba10e36fcf52fb002a338ff55ecb842d5282e0a6fb1226',
+        address: process.env.CONTRACT_OGAME,
     })
 }
