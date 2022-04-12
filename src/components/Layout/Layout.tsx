e12ff882753578d3ed927e1f73dfb7f3b96df23f
starwalker00 <aaronwalkerup@gmail.com>
2022-04-12 22:05:17 +0200

    other: change page title

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: any) {
     return (
         <>
             <Head>
-                <title>Template</title>
+                <title>Ogame on Starknet</title>
             </Head>
             <TransactionListStatus />
             <main>{children}</main>
